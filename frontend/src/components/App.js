c0d2152c705bc9f9094248d8dd4f483ac305a414
Phien Pham <phien@triip.me>
2017-10-31 23:47:36 +0700

    Add App component to glue things together

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+import { Switch, Route, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { getCategories } from '../actions/category';
+import { getPostsAPI } from '../actions/post';
+import Navigation from './Navigation';
+import PostList from './post/PostList';
+import PostForm from './post/PostForm';
+import PostDetails from './post/PostDetails';
+
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.getCategories();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <Navigation />
+        <Switch>
+          <Route exact path="/" component={PostList} />
+          <Route exact path="/new" component={PostForm} />
+          <Route exact path="/:category" component={PostList} />
+          <Route path="/:category/:postId" component={PostDetails} />
+        </Switch>
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  getCategories: PropTypes.func,
+  getPosts: PropTypes.func,
+};
+
+export default  withRouter(connect(
+  null,
+  {
+    getCategories: getCategories,
+    getPosts: getPostsAPI
+  }
+)(App));
