056db5dc388f1380beca3c42113975c16f3fbda2
Phien Pham <phien@triip.me>
2017-10-31 13:13:08 +0700

    Wrap App component with BrowserRouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import combineReducers from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  combineReducers,
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
